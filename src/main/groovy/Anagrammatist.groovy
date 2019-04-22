2c602649dc809e5bb9b1bd3e480fea89d46468e2
Miguel Pagola <mipagola@gmail.com>
2019-04-22 16:00:36 +0200

    I/O word loop

--- src/main/groovy/Anagrammatist.groovy
+++ src/main/groovy/Anagrammatist.groovy
@@ -5,6 +5,8 @@ class Anagrammatist {
             String dictionary = args[0]
             println "Dictionary folder $dictionary will be loaded"
 
+            searchForAnagrams()
+
         } else {
             println """
 Use: 
@@ -18,5 +20,21 @@ Use:
 
     static boolean dictionaryFolderIsReceived(String... args) {
         1 == args.size()
+    }
+
+    static def searchForAnagrams() {
+        println 'Use <ctrl> + C to finish'
+
+        def (query, console) = [
+                '* Write a word for searching anagrams: ',
+                new Scanner(new InputStreamReader(System.in, "UTF-8")),
+        ]
+
+        while (true) {
+            print query
+            String input = console.nextLine()
+
+            println "Longest anagram is ... $input"
+        }
     }
 }
